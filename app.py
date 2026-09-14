f97ab8efca3a116d40600ca71649b74132b8464e
safamariyam024-ui <safamariyam024@gmail.com>
2026-09-14 21:13:35 +0400

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ scenario = st.sidebar.radio(
 )
 
 image_files = {
-    "Small Crack": "crack_images/small_crack.jpg.jpeg",
-    "Medium Crack": "crack_images/medium_crack.jpg.jpeg",
-    "Large Crack": "crack_images/large_crack.jpg.jpeg"
+     "Small Crack": "crack_images/small_crack.jpeg",
+     "Medium Crack": "crack_images/medium_crack.jpeg",
+     "Large Crack": "crack_images/large_crack.jpeg"
 }
 
 image_path = image_files[scenario]
